dd39b3bc9ee08eb3701db24b0cd68e4328f654dc
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-11 21:24:52 -0400

    commit type

---
+++ pointer/type.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void data_type() {
+    /*
+    %c --> character
+    %d --> integer
+    %f --> float
+    %i --> signed integer
+    %ld --> Long
+    %lf --> Double
+    %lu --> unsigned integer and unsigned long
+    %p --> pointer
+    %s --> string
+    %u --> unsigned int
+    */
+
+   printf("%s\n", "Done!");
+}
+
+
+int main() {
+    data_type();
+    return 0;
+}
