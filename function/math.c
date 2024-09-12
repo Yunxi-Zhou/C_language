16cd1ad62ba85ab1123aa8e14be9b3ebc4447aa8
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-12 00:09:59 -0400

    commit math function

---
+++ function/math.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+
+    printf("%f\n", sqrt(16));
+    printf("%f\n", ceil(1.4)); //ceil() upwards to its nearest integer
+    printf("%f\n", floor(1.4)); //floor() downwards to its nearest integer
+    printf("%f\n", pow(4,3)); // 4^3
+
+    return 0;
+}
