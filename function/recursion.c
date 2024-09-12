ed5a260e39e5958139cb3aee0468199859c59e42
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-11 23:59:34 -0400

    commit recursion thinking

---
+++ function/recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//function declaration
+int rec_sum(int k);
+
+int main() {
+    int i = rec_sum(10);
+    printf("%d\n", i);
+    return 0;
+}
+
+int rec_sum(int k) {
+    if (k > 0) {
+        return k + rec_sum(k-1);
+    } else {
+        return 0;
+    }
+}
